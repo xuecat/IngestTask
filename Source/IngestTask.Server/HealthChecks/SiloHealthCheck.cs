b1211c1ebf9251e7b8f3687ac99903a8e359d428
wangqiu <wangqiu@sobey.com>
2021-03-12 16:57:41 +0800

    临时修改ha方案

--- Source/IngestTask.Server/HealthChecks/SiloHealthCheck.cs
+++ Source/IngestTask.Server/HealthChecks/SiloHealthCheck.cs
@@ -10,7 +10,7 @@ namespace IngestTask.Server.HealthChecks
   
     public class SiloHealthCheck : IHealthCheck
     {
-        private static long lastCheckTime = DateTime.UtcNow.ToBinary();
+        //private static long lastCheckTime = DateTime.UtcNow.ToBinary();
         private readonly IEnumerable<IHealthCheckParticipant> participants;
 
         public SiloHealthCheck(IEnumerable<IHealthCheckParticipant> participants) => this.participants = participants;
@@ -19,17 +19,17 @@ namespace IngestTask.Server.HealthChecks
         {
             return Task.FromResult(HealthCheckResult.Healthy());
             /*暂时先这样避免ha重启*/
-            var thisLastCheckTime = DateTime.FromBinary(Interlocked.Exchange(ref lastCheckTime, DateTime.UtcNow.ToBinary()));
+            //var thisLastCheckTime = DateTime.FromBinary(Interlocked.Exchange(ref lastCheckTime, DateTime.UtcNow.ToBinary()));
 
-            foreach (var participant in this.participants)
-            {
-                if (!participant.CheckHealth(thisLastCheckTime))
-                {
-                    return Task.FromResult(HealthCheckResult.Degraded());
-                }
-            }
+            //foreach (var participant in this.participants)
+            //{
+            //    if (!participant.CheckHealth(thisLastCheckTime))
+            //    {
+            //        return Task.FromResult(HealthCheckResult.Degraded());
+            //    }
+            //}
 
-            return Task.FromResult(HealthCheckResult.Healthy());
+            //return Task.FromResult(HealthCheckResult.Healthy());
         }
     }
 }
