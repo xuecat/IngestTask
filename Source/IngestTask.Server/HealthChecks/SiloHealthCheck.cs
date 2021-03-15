c1e29336c219912fcc1d0127c2fa88facc0dddae
wangqiu <wangqiu@sobey.com>
2021-03-15 19:34:24 +0800

    回复检测

--- Source/IngestTask.Server/HealthChecks/SiloHealthCheck.cs
+++ Source/IngestTask.Server/HealthChecks/SiloHealthCheck.cs
@@ -10,26 +10,26 @@ namespace IngestTask.Server.HealthChecks
   
     public class SiloHealthCheck : IHealthCheck
     {
-        //private static long lastCheckTime = DateTime.UtcNow.ToBinary();
+        private static long lastCheckTime = DateTime.UtcNow.ToBinary();
         private readonly IEnumerable<IHealthCheckParticipant> participants;
 
         public SiloHealthCheck(IEnumerable<IHealthCheckParticipant> participants) => this.participants = participants;
 
         public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
-            return Task.FromResult(HealthCheckResult.Healthy());
-            /*暂时先这样避免ha重启*/
-            //var thisLastCheckTime = DateTime.FromBinary(Interlocked.Exchange(ref lastCheckTime, DateTime.UtcNow.ToBinary()));
-
-            //foreach (var participant in this.participants)
-            //{
-            //    if (!participant.CheckHealth(thisLastCheckTime))
-            //    {
-            //        return Task.FromResult(HealthCheckResult.Degraded());
-            //    }
-            //}
-
             //return Task.FromResult(HealthCheckResult.Healthy());
+            /*暂时先这样避免ha重启*/
+            var thisLastCheckTime = DateTime.FromBinary(Interlocked.Exchange(ref lastCheckTime, DateTime.UtcNow.ToBinary()));
+
+            foreach (var participant in this.participants)
+            {
+                if (!participant.CheckHealth(thisLastCheckTime))
+                {
+                    return Task.FromResult(HealthCheckResult.Degraded());
+                }
+            }
+
+            return Task.FromResult(HealthCheckResult.Healthy());
         }
     }
 }
